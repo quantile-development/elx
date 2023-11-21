865b3ec4607d1aea0a7436cfebdfefd1d2caa5aa
BernardWez <bernard.wezeman@quantile.nl>
2023-11-21 18:42:21 +0100

    Remove fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 from fixtures.singer import singer
-from fixtures.tap import tap, tap_incremental, tap_multiple_streams
+from fixtures.tap import tap
 from fixtures.target import target
-from fixtures.runner import runner, runner_with_deselected_stream
+from fixtures.runner import runner
 from fixtures.state import state_manager
