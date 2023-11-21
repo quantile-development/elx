389268e419d9b900a77fca63b697c713dbdb4748
BernardWez <bernard.wezeman@quantile.nl>
2023-11-21 18:41:54 +0100

    Use custom tap-mock-fixture for testing

--- tests/fixtures/tap.py
+++ tests/fixtures/tap.py
@@ -6,55 +6,10 @@ from elx import Tap
 @pytest.fixture(scope="session")
 def tap() -> Generator[Tap, None, None]:
     """
-    Return a Tap instance for the tap-smoke-test executable.
-    """
-    yield Tap(
-        executable="tap-smoke-test",
-        spec="git+https://github.com/meltano/tap-smoke-test.git",
-        config={
-            "streams": [
-                {
-                    "stream_name": "animals",
-                    "input_filename": "https://gitlab.com/meltano/tap-smoke-test/-/raw/main/demo-data/animals-data.jsonl",
-                },
-            ],
-        },
-    )
-
-
-@pytest.fixture(scope="session")
-def tap_incremental() -> Generator[Tap, None, None]:
-    """
     Return a Tap instance for the  executable with an incremental stream.
     """
     yield Tap(
-        executable="tap-mock-incremental",
-        spec="git+https://github.com/quantile-taps/tap-mock-incremental.git",
+        executable="tap-mock-fixture",
+        spec="git+https://github.com/quantile-taps/tap-mock-fixture.git",
         config={},
-    )
-
-
-@pytest.fixture(scope="session")
-def tap_multiple_streams() -> Generator[Tap, None, None]:
-    """
-    Return a Tap instance for the tap-smoke-test executable with two streams.
-
-    One stream is selected and one stream is deselected.
-    """
-    yield Tap(
-        executable="tap-smoke-test",
-        spec="git+https://github.com/meltano/tap-smoke-test.git",
-        config={
-            "streams": [
-                {
-                    "stream_name": "animals",
-                    "input_filename": "https://gitlab.com/meltano/tap-smoke-test/-/raw/main/demo-data/animals-data.jsonl",
-                },
-                {
-                    "stream_name": "animals-two",
-                    "input_filename": "https://gitlab.com/meltano/tap-smoke-test/-/raw/main/demo-data/animals-data.jsonl",
-                },
-            ],
-        },
-        deselected=["animals-two"],
     )
