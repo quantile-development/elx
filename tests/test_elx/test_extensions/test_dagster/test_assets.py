93ea1392ada327293c8999c6bb3f51f626b6c551
BernardWez <bernard.wezeman@quantile.nl>
2023-11-21 14:47:01 +0100

    Add asset loading test for deselected stream

--- tests/test_elx/test_extensions/test_dagster/test_assets.py
+++ tests/test_elx/test_extensions/test_dagster/test_assets.py
@@ -10,3 +10,15 @@ def test_asset_loading(runner: Runner):
     assets = load_assets(runner)
     assert len(assets) == 1
     assert isinstance(assets[0], AssetsDefinition)
+
+
+def test_asset_loading_with_deselected_stream(runner_with_deselected_stream: Runner):
+    """
+    Test that assets are loaded correctly.
+    """
+    # Verifies that the associated tap has multiple streams
+    assert len(runner_with_deselected_stream.tap.catalog.streams) == 2
+
+    assets = load_assets(runner_with_deselected_stream)
+    assert len(assets) == 1
+    assert isinstance(assets[0], AssetsDefinition)
