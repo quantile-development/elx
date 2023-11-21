e78712d770ed085a681dfabd11c9c8ba46cf96f2
BernardWez <bernard.wezeman@quantile.nl>
2023-11-21 18:42:33 +0100

    Update runner fixture

--- tests/fixtures/runner.py
+++ tests/fixtures/runner.py
@@ -8,25 +8,10 @@ from .target import target
 @pytest.fixture
 def runner(tmp_path, tap: Tap, target: Target) -> Generator[Runner, None, None]:
     """
-    Return a Runner instance for the tap-smoke-test executable.
+    Return a Runner instance for the tap-mock-fixture executable.
     """
     yield Runner(
         tap=tap,
-        target=target,
-        state_manager=StateManager(base_path=str(tmp_path)),
-    )
-
-
-@pytest.fixture
-def runner_with_deselected_stream(
-    tmp_path, tap_multiple_streams: Tap, target: Target
-) -> Generator[Runner, None, None]:
-    """
-    Return a Runner instance for the tap-smoke-test executable with two streams,
-    of which one is deselected.
-    """
-    yield Runner(
-        tap=tap_multiple_streams,
         target=target,
         state_manager=StateManager(base_path=str(tmp_path)),
     )
